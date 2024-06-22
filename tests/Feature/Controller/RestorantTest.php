dcea7ea852cd5173d6265edd1beeac391352d449
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-22 22:54:07 +0700

    feat: test case untuk restorant controller get current restorant

---
+++ tests/Feature/Controller/RestorantTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\Controller;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RestorantTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function user_token_generate()
+    {
+        $password = '123456';
+        $user = \App\Models\User::factory()->create([
+            'name' => fake()->name,
+            'email' => fake()->unique()->email,
+            'phone' => fake()->unique()->phoneNumber,
+            'password' => $password
+        ]);
+
+        $token = $user->createToken('test-token')->plainTextToken;
+
+        return $token;
+    }
+
+    public function test_current_restorant_success()
+    {
+        // create user with role restorant
+        $user = \App\Models\User::factory()->has(\App\Models\Restorant::factory())->create([
+            'role' => 'restorant'
+        ]);
+
+        $token = $user->createToken('test-token')->plainTextToken;
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])
+        ->getJson('/api/restorant', [])
+        ->assertStatus(200);
+    }
+
+    public function test_current_restorant_failed()
+    {
+        $token = $this->user_token_generate();
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])
+        ->getJson('/api/restorant', [])
+        ->assertStatus(404);
+    }
+
+    public function test_current_restorant_failed_without_auth()
+    {
+        $this->getJson('/api/restorant', [])
+        ->assertStatus(401);
+    }
+
+    
+}
