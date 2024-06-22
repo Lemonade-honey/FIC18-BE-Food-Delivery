50f779c75c1844b3b157baafc6c8aa1009538823
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-22 23:33:51 +0700

    feat: test case untuk create restorant

--- tests/Feature/Controller/RestorantTest.php
+++ tests/Feature/Controller/RestorantTest.php
@@ -4,11 +4,34 @@ namespace Tests\Feature\Controller;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class RestorantTest extends TestCase
 {
     use RefreshDatabase;
+
+    private static function generate_user_with_restorant(): \App\Models\User
+    {
+        $user = \App\Models\User::factory()->has(\App\Models\Restorant::factory())->create([
+            'role' => 'restorant'
+        ]);
+
+        return $user;
+    }
+
+    /**
+     * Dummy Data Restorant
+     */
+    private function dummy_restorant_valid_data(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'address' => fake()->address(),
+            'latlong' => fake()->latitude() .','. fake()->longitude(),
+            'photo' => UploadedFile::fake()->image('test.jpg')
+        ];
+    }
 
     public function test_current_restorant_success()
     {
@@ -27,7 +50,7 @@ class RestorantTest extends TestCase
         ->assertStatus(200);
     }
 
-    public function test_current_restorant_failed()
+    public function test_current_restorant_failed_no_data()
     {
         $token = $this->user_token_generate();
 
@@ -45,5 +68,51 @@ class RestorantTest extends TestCase
         ->assertStatus(401);
     }
 
-    
+    public function test_create_restorant_success()
+    {
+        $token = $this->user_token_generate();
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])
+        ->postJson('/api/restorant/create', $this->dummy_restorant_valid_data())
+        ->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => ['id', 'name', 'latlong', 'photo']
+        ]);
+    }
+
+    public function test_create_restorant_failed_already_has_restorant_data()
+    {
+        $user = $this->generate_user_with_restorant();
+
+        $token = $token = $user->createToken('test-token')->plainTextToken;
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])
+        ->postJson('/api/restorant/create', $this->dummy_restorant_valid_data())
+        ->assertStatus(409)
+        ->assertJsonValidationErrors(['massage']);
+    }
+
+    public function test_create_restorant_failed_validation()
+    {
+        $token = $this->user_token_generate();
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ])
+        ->postJson('/api/restorant/create', [
+            'name' => '',
+            'address' => fake()->address(),
+            'latlong' => '',
+            'photo' => ''
+        ])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['name', 'latlong', 'photo']);
+    }
 }
