8c7340909f65b097518f42eec1486903d1916b20
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-24 12:42:43 +0700

    feat: test case untuk repo container restorant

---
+++ tests/Feature/ContainerRestorantRepoTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use App\Models\Restorant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContainerRestorantRepoTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $restorantRepo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->restorantRepo = $this->app->make(\App\Repositorys\Interface\RestorantRepository::class);
+    }
+
+    public function test_get_current_restorant()
+    {
+        $user = \App\Models\User::factory()->has(Restorant::factory())->create();
+
+        $result = $this->restorantRepo->getCurrentRestorantByUser($user->id);
+
+        $this->assertInstanceOf(Restorant::class, $result);
+    }
+    public function test_get_current_restorant_with_products()
+    {
+        $user = \App\Models\User::factory()->has(Restorant::factory()->has(Product::factory()))->create();
+
+        $result = $this->restorantRepo->getCurrentRestorantWithProducts($user->id);
+
+        $this->assertInstanceOf(Restorant::class, $result);
+    }
+}
