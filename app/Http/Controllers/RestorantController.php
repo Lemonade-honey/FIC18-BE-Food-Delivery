3186915d28eb96902b574e5f5472aa483a85a645
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-24 13:14:34 +0700

    feat: restorant product dan create product done

--- app/Http/Controllers/RestorantController.php
+++ app/Http/Controllers/RestorantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Restorant;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -101,6 +102,111 @@ class RestorantController extends Controller
                 ]
             ], 504);
         }
+    }
+
+    public function currentRestorantProducts(Request $request)
+    {
+        $user = $request->user();
+
+        $restorant = Restorant::with('products')->where('user_id', $user->id)->first();
+
+        if (! $restorant)
+        {
+            return self::errorResponseRestorantNotFound();
+        }
+
+        if ($restorant->products()->count() == 0)
+        {
+            return response()->json([
+                'massage' => 'tidak ada product terdaftar'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $restorant->products
+        ]);
+    }
+
+    public function currentRestorantCreateProduct(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255', 'min:3'],
+            'image' => ['required', 'image', 'max:10280'],
+            'deskripsi' => 'required',
+            'type' => ['required', 'in:makanan,minuman'],
+            'harga' => ['required', 'numeric']
+        ]);
+
+        $user = $request->user();
+
+        try{
+            $userRestorant = $this->restorantRepo->getCurrentRestorantByUser($user->id);
+
+            if (! $userRestorant)
+            {
+                return self::errorResponseRestorantNotFound();
+            }
+
+            $product = Product::create([
+                'restorant_id' => $userRestorant->id,
+                'name' => $request->input('name'),
+                'image' => $this->fileService->saveFileToStoragePath($request->file('image'), 'products/test'),
+                'deskripsi' => $request->input('deskripsi'),
+                'type' => $request->input('type'),
+                'harga' => $request->input('harga')
+            ]);
+
+            return response()->json([
+                'data' => $product
+            ], 201);
+        }
+
+        catch (Throwable $th){
+
+            Log::critical('user gagal membuat product. Error Code : ' . $th->getCode(), [
+                'class' => get_class(),
+                'massage' => $th->getMessage()
+            ]);
+
+            return self::errorResponseServerError();
+        }
+    }
+
+    public function currentRestorantProduct(Request $request, $id)
+    {
+        $user = $request->user();
+
+        $userRestorant = $this->restorantRepo->getCurrentRestorantByUser($user->id);
+
+        if (! $userRestorant)
+        {
+            return self::errorResponseRestorantNotFound();
+        }
+
+        $product = Product::where(['restorant_id' => $userRestorant->id, 'id' => $id])->first();
+
+        if (! $product)
+        {
+            return response()->json([
+                'massage' => 'product tidak ditemukan'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $product
+        ]);
+    }
+
+    /**
+     * Static response for restorant not found
+     * 
+     * no access for outside resorce
+     */
+    private static function errorResponseRestorantNotFound(): JsonResponse
+    {
+        return response()->json([
+            'massage' => 'restorant user not found'
+        ], 404);
     }
 
 }
