5aee552cfa65743e94053f427de444df377dc650
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-25 21:52:45 +0700

    feat: restorant by id

--- app/Http/Controllers/RestorantController.php
+++ app/Http/Controllers/RestorantController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Restorant;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RestorantController extends Controller
@@ -15,12 +16,26 @@ class RestorantController extends Controller
         $this->restorantService = $restorantService;
     }
 
-    public function restorants(Request $request)
+    public function restorants(Request $request): JsonResponse
     {
         $restorantsPaginate = $this->restorantService->restorantsByNameOrProducts($request);
 
         return response()->json([
             $restorantsPaginate
+        ]);
+    }
+
+    public function restorant($id): JsonResponse
+    {
+        $restorant = $this->restorantService->restorantWithProductByRestorantId($id);
+
+        if (! $restorant)
+        {
+            return self::errorResponseDataNotFound();
+        }
+
+        return response()->json([
+            'data' => $restorant 
         ]);
     }
 }
