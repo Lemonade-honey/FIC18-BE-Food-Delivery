f82889cacc8c143514db5f651fc1c1fdbb5bbe51
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-25 21:13:11 +0700

    test: test case untuk restorants name dan products

--- tests/Feature/ServiceContainer/RestorantServiceTest.php
+++ tests/Feature/ServiceContainer/RestorantServiceTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\ServiceContainer;
 
+use App\Models\Product;
 use App\Models\Restorant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class RestorantServiceTest extends TestCase
@@ -24,29 +26,29 @@ class RestorantServiceTest extends TestCase
         return \App\Models\User::factory()->create();
     }
 
-    private function generate_user_restorant()
+    private function generate_user_restorant(int $total = 1)
     {
-        return \App\Models\User::factory()->has(\App\Models\Restorant::factory())->create();
+        return \App\Models\User::factory($total)->has(Restorant::factory())->create();
     }
 
     public function test_container_services(){
         $this->assertTrue(true);
     }
 
-    public function test_restorant_user_by_request_not_null()
-    {
-        $user = $this->generate_user_restorant();
+    // public function test_restorant_user_by_request_not_null()
+    // {
+    //     $user = $this->generate_user_restorant();
 
-        $request = \Illuminate\Http\Request::create('/some-url', 'GET');
-        $request->setUserResolver(function () use ($user) {
-            return $user;
-        });
+    //     $request = \Illuminate\Http\Request::create('/some-url', 'GET');
+    //     $request->setUserResolver(function () use ($user) {
+    //         return $user;
+    //     });
 
-        $service = $this->restorantService->restorantUserByRequest($request);
+    //     $service = $this->restorantService->restorantUserByRequest($request);
 
-        $this->assertNotNull($service);
-        $this->assertInstanceOf(Restorant::class, $service);
-    }
+    //     $this->assertNotNull($service);
+    //     $this->assertInstanceOf(Restorant::class, $service);
+    // }
 
     public function test_restorant_user_by_request_null()
     {
@@ -60,5 +62,49 @@ class RestorantServiceTest extends TestCase
         $service = $this->restorantService->restorantUserByRequest($request);
 
         $this->assertNull($service);
+    }
+
+    public function test_restorants_name_or_products()
+    {
+        $this->generate_user_restorant(3);
+
+        $request = \Illuminate\Http\Request::create('/some-url', 'GET');
+
+        $service = $this->restorantService->restorantsByNameOrProducts($request);
+
+        $this->assertTrue($service->total() == 3, $service->total());
+    }
+
+    public function test_restorants_name_or_products_search_restorant_name()
+    {
+
+        Restorant::factory()->create([
+            'name' => 'preksu penyet'
+        ]);
+
+        $this->generate_user_restorant(2);
+
+        $request = \Illuminate\Http\Request::create('/some-url', 'GET', ['search' => 'preksu penyet']);
+
+        $service = $this->restorantService->restorantsByNameOrProducts($request);
+
+        $this->assertTrue($service->total() == 1, $service->total());
+    }
+
+    public function test_restorants_name_or_products_search_products_name()
+    {
+        $restorants = Restorant::factory()->create();
+        $products = Product::factory()->create([
+            'restorant_id' => $restorants->id,
+            'name' => 'red valved'
+        ]);
+
+        $this->generate_user_restorant(3);
+
+        $request = \Illuminate\Http\Request::create('/some-url', 'GET', ['search' => 'red valved']);
+
+        $service = $this->restorantService->restorantsByNameOrProducts($request);
+
+        $this->assertTrue($service->total() == 1, $service->total());
     }
 }
