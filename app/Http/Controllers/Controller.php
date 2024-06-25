bc836e05d63dd2571d25b46ba26079cb63659646
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-25 21:45:15 +0700

    feat: global massage untuk 404 response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
     /**
      * Static response for server error
      */
-    static function errorResponseServerError(): \Illuminate\Http\JsonResponse
+    static function errorResponseServerError(): JsonResponse
     {
         return response()->json([
             'massage' => 'server erros'
         ], 504);
+    }
+
+    /**
+     * Static Respone for data not found
+     */
+    static function errorResponseDataNotFound(): JsonResponse
+    {
+        return response()->json([
+            'massage' => 'data not found'
+        ], 404);
     }
 }
