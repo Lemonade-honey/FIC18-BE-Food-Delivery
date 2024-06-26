aa327ede821ca406931b64beacef31d2f33a7e63
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-26 11:39:35 +0700

    fix: perubahan factory data

--- database/factories/RestorantFactory.php
+++ database/factories/RestorantFactory.php
@@ -18,9 +18,11 @@ class RestorantFactory extends Factory
     {
         return [
             'user_id' => \App\Models\User::factory(),
-            'name' => fake()->company(),
+            'name' => fake()->company() . ' dummy',
+            'tags' => [],
             'address' => fake()->address(),
-            'latlong' => fake()->latitude() .','. fake()->longitude()
+            'latlong' => fake()->latitude() .','. fake()->longitude(),
+            'image' => 'https://png.pngtree.com/png-vector/20190721/ourlarge/pngtree-flat-restaurant-illustration-png-image_1568130.jpg'
         ];
     }
 }
