11ee559403dde388e4a70e4d4ede1e64b1b7e4cb
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-28 20:50:21 +0700

    feat: register order service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(\App\Services\Interfaces\FileService::class, \App\Services\FileServiceImpl::class);
         $this->app->bind(\App\Services\Interfaces\RestorantService::class, \App\Services\RestorantServiceImpl::class);
         $this->app->bind(\App\Services\Interfaces\ProductService::class, \App\Services\ProductServiceImpl::class);
+        $this->app->bind(\App\Services\Interfaces\OrderService::class, \App\Services\OrderServiceImpl::class);
     }
 
     /**
