9bf8ac179b0c7be4d4b6687cd6cdddc728b12f8f
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-07-03 21:24:14 +0700

    feat: relasi user dengan order

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Restorant::class);
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
