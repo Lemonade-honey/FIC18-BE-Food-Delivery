9c92b4245e7c80fac4ed21a53bd8a4b0a6756a43
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-07-03 11:25:39 +0700

    feat: register flip gate service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(\App\Services\Interfaces\RestorantService::class, \App\Services\RestorantServiceImpl::class);
         $this->app->bind(\App\Services\Interfaces\ProductService::class, \App\Services\ProductServiceImpl::class);
         $this->app->bind(\App\Services\Interfaces\OrderService::class, \App\Services\OrderServiceImpl::class);
+        $this->app->bind(\App\Services\Interfaces\FlipGateService::class, \App\Services\FlipGateServiceImpl::class);
     }
 
     /**
